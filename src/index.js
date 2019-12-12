43ff1061b1630035af7544f770b9bfb46b1d151d
Soraia <soraia@Soraias-iMac.local>
2019-12-12 13:03:09 -0500

    set up reducer, state and created store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+
+import { reducer } from '../src/reducers/reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+// console.log(store);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+rootElement);
